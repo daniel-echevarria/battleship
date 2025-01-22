20b23cc700a5f950aaa7384ca2c3b3682c7a2bfe
Kasper Bak <dan.echevarri@gmail.com>
2025-01-22 11:14:17 +0100

    Add shipClasses file and data

---
+++ src/data/shipClasses.ts
@@ -0,0 +1,12 @@
+type ShipClass = {
+  name: string;
+  size: number;
+};
+
+const shipClasses: ShipClass[] = [
+  { name: "Langskip", size: 5 },
+  { name: "Drakkar", size: 4 },
+  { name: "Knarr", size: 3 },
+  { name: "Karve", size: 3 },
+  { name: "Snekja", size: 2 },
+];
