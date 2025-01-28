97c8fef54c2ecc3724c14ae0702adfaa1f4cc750
Kasper Bak <dan.echevarri@gmail.com>
2025-01-28 22:51:53 +0100

    Refactor generateCoordinates to accept an xLength and yLength as arguments

--- src/utils/generateCoordinates.test.ts
+++ src/utils/generateCoordinates.test.ts
@@ -1,17 +1,21 @@
 import generateCoordinates from "./generateCoordinates";
 
 describe("generateCoordinates", () => {
-  describe("when passed a board size of 2", () => {
+  describe("when passed a xLength of 2 and an yLength of 2", () => {
     it("returns an array of ['a1, 'b1', 'a2', 'b2']", () => {
-      const boardSize = 2;
+      const xLength = 2;
+      const yLength = 2;
       const result = ["a1", "b1", "a2", "b2"];
-      expect(generateCoordinates(boardSize).sort()).toEqual(result.sort());
+      expect(generateCoordinates(xLength, yLength).sort()).toEqual(
+        result.sort()
+      );
     });
   });
 
-  describe("when passed a board size of 4", () => {
+  describe("when passed an xLength of 4 and an yLength of 4", () => {
     it("returns an array of ['a1', 'b1', 'c1', 'd1', 'a2', 'b2', 'c2', 'd2', 'a3', 'b3', 'c3', 'd3', 'a4', 'b4', 'c4', 'd4']", () => {
-      const boardSize = 4;
+      const xLength = 4;
+      const yLength = 4;
       const result = [
         "a1",
         "b1",
@@ -30,18 +34,32 @@ describe("generateCoordinates", () => {
         "c4",
         "d4",
       ];
-      expect(generateCoordinates(boardSize).sort()).toEqual(result.sort());
+      expect(generateCoordinates(xLength, yLength).sort()).toEqual(
+        result.sort()
+      );
     });
   });
 
-  describe("when passed a board size of 10", () => {
+  describe("when passed xLength of 10 and an yLength of 10", () => {
     it("returns a list that has 100 elements which include a1, f6 and j10", () => {
-      const boardSize = 10;
+      const xLength = 10;
+      const yLength = 10;
       const valuesToCheck = ["a1", "f6", "j10"];
-      expect(generateCoordinates(boardSize)).toHaveLength(100);
+      expect(generateCoordinates(xLength, yLength)).toHaveLength(100);
       valuesToCheck.forEach((value) => {
-        expect(generateCoordinates(boardSize)).toContain(value);
+        expect(generateCoordinates(xLength, yLength)).toContain(value);
       });
+    });
+  });
+
+  describe("when passed xLength of 3 and an yLength of 2", () => {
+    it("returns a array like ", () => {
+      const xLength = 3;
+      const yLength = 2;
+      const result = ["a1", "b1", "c1", "a2", "b2", "c2"];
+      expect(generateCoordinates(xLength, yLength).sort()).toEqual(
+        result.sort()
+      );
     });
   });
 });
--- src/utils/generateCoordinates.ts
+++ src/utils/generateCoordinates.ts
@@ -1,8 +1,8 @@
-const generateCoordinates = (boardSize: number) => {
+const generateCoordinates = (xLength: number, yLength: number) => {
   let possibles = [];
-  for (let i = 0; i < boardSize; i++) {
+  for (let i = 0; i < xLength; i++) {
     const letter = String.fromCharCode(i + 97);
-    for (let j = 0; j < boardSize; j++) {
+    for (let j = 0; j < yLength; j++) {
       const num = (j + 1).toString();
       const coordinate = [letter, num].join("");
       possibles.push(coordinate);
