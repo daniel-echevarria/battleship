1a797e8bc011004e999814fbadf0428ca532955d
Kasper Bak <dan.echevarri@gmail.com>
2025-02-12 12:50:17 +0100

    Prepare the method to handle the edge case of an empty array

--- src/utils/selectRandomCoordinate.test.ts
+++ src/utils/selectRandomCoordinate.test.ts
@@ -1,11 +1,18 @@
 import selectRandomCoordinate from "./selectRandomCoordinate";
 
 describe("selectRandomCoordinate", () => {
-  describe("when given an array of coordinates", () => {
-    const coordinates = ["a1", "b1", "c1", "d1"];
-    const result = selectRandomCoordinate(coordinates);
-    it("randomly selects one", () => {
-      expect(coordinates).toContain(result);
-    });
+  let coordinates: string[];
+
+  coordinates = ["a1", "b1", "c1", "d1"];
+  const result = selectRandomCoordinate(coordinates);
+  it("returns a value from the given array", () => {
+    expect(coordinates).toContain(result);
+  });
+
+  it("gracefully handles empty arrays", () => {
+    coordinates = [];
+    expect(() => selectRandomCoordinate(coordinates)).toThrowError(
+      "Array cannot be empty"
+    );
   });
 });
--- src/utils/selectRandomCoordinate.ts
+++ src/utils/selectRandomCoordinate.ts
@@ -3,6 +3,9 @@ function getRandomInt(min: number, max: number) {
 }
 
 const selectRandomCoordinate = (coordinates: string[]) => {
+  if (coordinates.length === 0) {
+    throw "Array cannot be empty";
+  }
   const randomIndex = getRandomInt(0, coordinates.length);
   return coordinates[randomIndex];
 };
