3b25a88726146b01f06d9e0734a7036b5ff1b59f
Kasper Bak <dan.echevarri@gmail.com>
2025-02-28 11:24:30 +0100

    Rename files to separate UI from models

---
+++ src/ui/Game/GameUI.tsx
@@ -0,0 +1,19 @@
+import setupBoardFactory from "@/models/setupBoard/setupBoard";
+import React from "react";
+import SetupBoard from "../SetupBoard/SetupBoardUI";
+
+interface GameProps {
+  boardSize: number;
+}
+
+const Game: React.FC<GameProps> = ({ boardSize }) => {
+  const mySetupBoardFactory = setupBoardFactory();
+  const mySetupBoard = mySetupBoardFactory(boardSize);
+  return (
+    <main className="flex m-auto">
+      <SetupBoard setupBoard={mySetupBoard} />
+    </main>
+  );
+};
+
+export default Game;
---
+++ src/ui/SetupBoard/SetupBoardUI.tsx
@@ -0,0 +1,66 @@
+import genBoardCoordinates from "@/utils/coordinatesGeneration/genBoardCoordinates";
+import React from "react";
+import Cell from "../Cell/Cell";
+import Ship from "../Ship/ShipUI";
+import shipClasses from "@/data/shipClasses";
+import genShipCoordinates from "@/utils/coordinatesGeneration/genShipCoordinates";
+
+// Algo for correctly show hovered cells:
+// On drag enter, calculate the potential starting coordinate of the ship
+// Calculate the hovered cells based on the length of the ship
+// Apply the hover effect to the cells
+// on drag leave, remove the hover effect from the cells
+
+const SetupBoard = ({ setupBoard }) => {
+  const [potentialShipStart, setPotentialShipStart] = React.useState("");
+  const [grabbedShipInfo, setGrabbedShipInfo] = React.useState({
+    grabOffset: null,
+    length: 0,
+    isVertical: true,
+  });
+
+  console.log(grabbedShipInfo);
+
+  const hoveredCells = genShipCoordinates({
+    length: grabbedShipInfo.length,
+    startCoordinate: potentialShipStart,
+    isVertical: true,
+  });
+
+  const isValidPosition = setupBoard.canShipGoThere({
+    startCoordinate: potentialShipStart,
+  });
+
+  const coordinates = genBoardCoordinates(setupBoard.size, setupBoard.size);
+  const cellList = coordinates.map((coo) => {
+    return (
+      <Cell
+        id={coo}
+        key={coo}
+        setPotentialShipStart={setPotentialShipStart}
+        grabbedShipInfo={grabbedShipInfo}
+        isHovered={hoveredCells.includes(coo)}
+      />
+    );
+  });
+
+  return (
+    <>
+      <Ship
+        shipClass={shipClasses[0]}
+        grabbedShipInfo={grabbedShipInfo}
+        setGrabbedShipInfo={setGrabbedShipInfo}
+      />
+      <Ship
+        shipClass={shipClasses[1]}
+        grabbedShipInfo={grabbedShipInfo}
+        setGrabbedShipInfo={setGrabbedShipInfo}
+      />
+      <div role="grid" className="grid grid-cols-10">
+        {cellList}
+      </div>
+    </>
+  );
+};
+
+export default SetupBoard;
