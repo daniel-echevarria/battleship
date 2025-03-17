2c096cb9038cb1582d91e637f64359ecf6297a81
Kasper Bak <dan.echevarri@gmail.com>
2025-03-17 22:43:38 +0100

    Change bg color

--- src/ui/Game/GameUI.tsx
+++ src/ui/Game/GameUI.tsx
@@ -26,7 +26,7 @@ const Game: React.FC<GameProps> = ({ boardSize }) => {
   });
 
   return (
-    <main className="flex border border-red-400 items-center justify-center h-screen">
+    <main className="flex border border-red-400 items-center justify-center h-screen bg-yellow-900">
       <SetupBoardUI player={playerOne} />
     </main>
   );
